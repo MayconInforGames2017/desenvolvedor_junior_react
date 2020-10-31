33acffc40d4cd31decf586562e07aab23c93dfe6
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-10-31 12:34:04 -0300

    criando primeiro componente

---
+++ cursos/src/TestCounter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TextCounter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Meu Computador</h1>
+                <textarea />
+                <div>
+                    <strong>Total:</strong>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TextCounter;
