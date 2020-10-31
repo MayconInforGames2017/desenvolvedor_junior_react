ce99d1e0cf8c37df16b0a5d36124a4b5bf64bc12
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-10-31 20:22:33 -0300

    componentes controlados

--- cursos/src/App.js
+++ cursos/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import TextCounter from './TestCounter';
+import MyForm from './MyForm';
 
 class App extends Component {
   render() {
     return (
       <div>
         <TextCounter limit={20}/>
+        <MyForm />
       </div>
     );
   }
---
+++ cursos/src/MyForm.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class MyForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    render() {
+        const { state } = this;
+        return (
+            <form>
+                <label>
+                    Name: 
+                    <input type="text" name="name" value={ state.name } onChange={ this.handleChange } /> { state.name }
+                </label>
+                <input type="submit" value="Enviar" />
+            </form>
+        )
+    }
+}
+
+export default MyForm;
