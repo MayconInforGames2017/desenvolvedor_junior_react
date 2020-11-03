5d3dfc5ac0cc69de51d2a0b946d20ad750c502fc
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-11-03 16:41:37 -0300

    criando componentes com classes

--- meu-app/src/components/MeuComponent.js
+++ meu-app/src/components/MeuComponent.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+/* 
 function MeuComponente() {
     return <h1>TreinaWeb</h1>;
+}
+*/
+
+class MeuComponente extends Component {
+    render() {
+        return <h1>TreinaWeb</h1>;
+    }    
 }
 
 export default MeuComponente;
